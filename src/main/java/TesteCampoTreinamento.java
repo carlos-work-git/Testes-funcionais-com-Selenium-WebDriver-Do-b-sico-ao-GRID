9c4141c3b381a3e2e8a38befb6ac193e0b82d1cc
José Carlos Junior <jose.carlos@to-brasil.local>
2023-04-18 19:40:31 -0300

    Tratando Testes Incompletos: Assert.fail() e @Ignore

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,15 +1,19 @@
-import javax.swing.JOptionPane;
+import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class TesteCampoTreinamento {
 	
 	@Test
+	@Ignore
 	public void testeTextField() {
 		
 		WebDriver driver = new FirefoxDriver(); // Firefox
@@ -29,7 +33,8 @@ public class TesteCampoTreinamento {
 	}
 	
 	@Test
-	public void teste_RadioButton() {
+	@Ignore
+	public void testandoOpcaoSelecionadaNoRadioButton() {
 		
 		WebDriver driver = new FirefoxDriver(); // Firefox
 		
@@ -48,7 +53,8 @@ public class TesteCampoTreinamento {
 	
 	
 	@Test
-	public void testeCheckBox() {
+	@Ignore
+	public void testeOpcaoSelecionadaNoCheckBox() {
 		
 		WebDriver driver = new FirefoxDriver(); // Firefox
 		
@@ -62,6 +68,104 @@ public class TesteCampoTreinamento {
 		String nomeTeste = "Teste CheckBox";
 		System.out.println(nomeTeste + " finish!");
 		driver.close();
+		
+	}
+	
+	@Test
+	@Ignore
+	public void testandoItemSelecionadoNoMenuDropDown() {
+		
+		WebDriver driver = new FirefoxDriver(); // Firefox
+		
+		// URL, Página do Projeto, acessada localmente, via arquivo HTML.
+		// System.getProperty("user.dir") é o "ApplicationPath" detectado automáticamente pela IDE Eclipse.
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade")); //Capturando o Menu DropDown
+		Select combo =  new Select(element);
+		//combo.selectByIndex(2);
+		//combo.selectByValue("superior");
+		combo.selectByVisibleText("2o grau completo");
+		
+		Assert.assertEquals("2o grau completo", combo.getFirstSelectedOption().getText() );
+		System.out.println(combo.getFirstSelectedOption().getText());
+		
+		//Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+		
+//		String nomeTeste = "Teste CheckBox";
+//		System.out.println(nomeTeste + " finish!");
+//		driver.close();
+		
+	}
+	
+
+	@Test
+	@Ignore
+	public void checandoItensDisponiveisNoMenuDropDown() {
+		
+		WebDriver driver = new FirefoxDriver(); // Firefox
+		
+		// URL, Página do Projeto, acessada localmente, via arquivo HTML.
+		// System.getProperty("user.dir") é o "ApplicationPath" detectado automáticamente pela IDE Eclipse.
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade")); //Capturando o Menu DropDown
+		Select combo =  new Select(element);
+		List<WebElement> options = combo.getOptions(); // Atribuindo opções do Menu DropDown à uma Lista de Elementos Web.
+		Assert.assertEquals(8, options.size());
+		
+		//Verificando se um elemento consta dentro do Menu DropDown
+		boolean encontrou = false;
+		
+		for(WebElement option:options) {
+			if(option.getText().equals("Mestrado")) {
+				encontrou = true;
+				System.out.println("Item Mestrado Encontrado!");
+				break;
+			}
+		}
+		
+		if (encontrou == false) {
+			System.out.println("Item Nao Encontrado!");
+		}
+		
+		Assert.assertTrue(encontrou);
+		
+	}
+	
+	@Test
+	@Ignore
+	public void checandoLinks() {
+		
+		WebDriver driver = new FirefoxDriver(); // Firefox
+		
+		// URL, Página do Projeto, acessada localmente, via arquivo HTML.
+		// System.getProperty("user.dir") é o "ApplicationPath" detectado automáticamente pela IDE Eclipse.
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.linkText("Voltar")).click();
+		System.out.println(driver.findElement(By.id("resultado")).getAttribute("value") );
+		
+		Assert.fail();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	@Ignore
+	public void AbrirPagina () {
+		
+		WebDriver driver = new FirefoxDriver(); // Firefox
+		
+		// URL, Página do Projeto, acessada localmente, via arquivo HTML.
+		// System.getProperty("user.dir") é o "ApplicationPath" detectado automáticamente pela IDE Eclipse.
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
 		
 	}
 	
